ac289566ced3e192bc93aa8c9d66103d9042832c
lain <lain@disroot.org>
2021-12-03 06:27:50 -0500

    use .env for token

--- battlerbot.py
+++ battlerbot.py
@@ -1,5 +1,10 @@
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.environ['DISCORD_TOKEN']
 
 bot = discord.Client()
 
-bot.run("OTE1MTMxMjI5NTEzMzI2NjMy.YaXIXw.LxZ28HxzG2oBYi4wOZZU4WuF9NU")
+bot.run(token)
